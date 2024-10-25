ed3112e7ca535a26b68cae32024043f5c0f9bd93
Tan Wei Jian <60432453+wj-tan@users.noreply.github.com>
2024-10-25 22:29:30 +0800

    added background running

--- db_vm/3_fastapi_conf.sh
+++ db_vm/3_fastapi_conf.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 cd /root/employee_db
-gunicorn -k uvicorn.workers.UvicornWorker app:app --bind 0.0.0.0:8000
+nohup gunicorn -k uvicorn.workers.UvicornWorker app:app --bind 0.0.0.0:8000 &
+
