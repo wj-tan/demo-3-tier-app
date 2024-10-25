a39e3616755dbb0872c8a77811cefdbd4d802508
Tan Wei Jian <60432453+wj-tan@users.noreply.github.com>
2024-10-25 22:50:01 +0800

    updated

--- db_vm/3_fastapi_conf.sh
+++ db_vm/3_fastapi_conf.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
-cd /root/employee_db
-nohup gunicorn -k uvicorn.workers.UvicornWorker app:app --bind 0.0.0.0:8000 &
+NAME=fastapi-mongodb-app
+DIR=/root/employee_db
+USER=root # This is the user id with which you are installing everything
+GROUP=root
+WORKERS=3
+WORKER_CLASS=uvicorn.workers.UvicornWorker
+BIND=unix:$DIR/run/gunicorn.sock
+LOG_LEVEL=error
+
+#cd /root/employee_db
+
+cd $DIR
+
+exec gunicorn app:app \
+  --workers $WORKERS \
+  --worker-class $WORKER_CLASS \
+  --user=$USER \
+  --group=$GROUP \
+  --bind=$BIND \
+  --log-level=$LOG_LEVEL \
+  --log-file=-
+#nohup gunicorn -k uvicorn.workers.UvicornWorker app:app --bind 0.0.0.0:8000 &
 
