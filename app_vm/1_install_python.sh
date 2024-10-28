2c252c90766d49d34666f8c81be6a1dc32f722de
Tan Wei Jian <60432453+wj-tan@users.noreply.github.com>
2024-10-28 14:36:14 +0800

    remove invisible spaces

--- app_vm/1_install_python.sh
+++ app_vm/1_install_python.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-apk add python3  --no-cache
+apk add python3 --no-cache
 python3 -m ensurepip  
 pip3 install --upgrade pip
-pip3 install flask  
+pip3 install flask 
 pip3 install -r requirements.txt
