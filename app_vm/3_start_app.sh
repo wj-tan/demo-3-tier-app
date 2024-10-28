9a782529e46725cbe504507f3b9d8c7dca32e22d
Tan Wei Jian <60432453+wj-tan@users.noreply.github.com>
2024-10-28 11:08:10 +0800

    Update 3_start_app.sh

--- app_vm/3_start_app.sh
+++ app_vm/3_start_app.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
 cd /root/employee_app
+
+# Find the Gunicorn process ID (PID)
+PID=$(pgrep gunicorn)
+
+# Check if Gunicorn is running
+if [ -n "$PID" ]; then
+    echo "Killing Gunicorn process with PID: $PID"
+    kill -9 $PID
+    echo "Gunicorn process terminated."
+else
+    echo "No Gunicorn process found."
+fi
+
 nohup /usr/bin/gunicorn --bind 0.0.0.0:8080 app:app  &
