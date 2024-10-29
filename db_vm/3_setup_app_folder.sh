9413df64070ce07017e52b9d45365c0a7b5298ef
Tan Wei Jian <60432453+wj-tan@users.noreply.github.com>
2024-10-29 18:32:21 +0800

    added missing /

--- db_vm/3_setup_app_folder.sh
+++ db_vm/3_setup_app_folder.sh
@@ -3,4 +3,4 @@
 mkdir /root/employee_db
 mkdir /root/employee_db/run
 mkdir /root/employee_db/logs
-cp -r /root/demo-3-tier-app/db_vm/* /root/employee_app
+cp -r /root/demo-3-tier-app/db_vm/* /root/employee_app/
