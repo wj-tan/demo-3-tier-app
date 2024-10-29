d437fd0095f86c3df8a927d5ede507046a5b5b1e
Tan Wei Jian <60432453+wj-tan@users.noreply.github.com>
2024-10-29 15:17:52 +0800

    added rhel commands

--- app_vm/1_install_python.sh
+++ app_vm/1_install_python.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# For Rhel
+# dnf install python3 -y
 apk add python3 --no-cache
 python3 -m ensurepip  
 pip3 install --upgrade pip
--- app_vm/2_setup_app_folder.sh
+++ app_vm/2_setup_app_folder.sh
@@ -3,3 +3,7 @@
 mkdir /root/employee_app
 mkdir /root/employee_app/logs
 cp -r /root/demo-3-tier-app/app_vm/* /root/employee_app
+
+# RHEL
+#sudo firewall-cmd --permanent --add-port=8080/tcp
+#sudo firewall-cmd --reload
