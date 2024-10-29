127e5cb49f23271f6fc42e38af0623ac75411540
Tan Wei Jian <60432453+wj-tan@users.noreply.github.com>
2024-10-29 19:45:08 +0800

    added firewall and selinux command

--- web_vm/2_config_nginx.sh
+++ web_vm/2_config_nginx.sh
@@ -5,3 +5,6 @@ cp /root/demo-3-tier-app/web_vm/default.conf /etc/nginx/http.d/default.conf
 
 echo "Remember to update the /etc/nginx/http.d/default.conf with the correct IPs"
 
+sudo firewall-cmd --permanent --add-port=80/tcp
+sudo firewall-cmd --permanent --add-port=8080/tcp
+sudo setsebool -P httpd_can_network_connect 1
