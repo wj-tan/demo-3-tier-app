b3afbbdebc22b7d26b77d8dde875f9112ed9d278
Tan Wei Jian <60432453+wj-tan@users.noreply.github.com>
2024-11-09 16:28:10 +0800

    Update 3_start_app.sh

--- app_vm/3_start_app.sh
+++ app_vm/3_start_app.sh
@@ -17,3 +17,5 @@ fi
 nohup gunicorn --bind 0.0.0.0:8080 app:app  &
 
 #nohup gunicorn -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8080 app:app --access-logfile /root/employee_app/logs/gunicorn-access.log --error-logfile /root/employee_app/logs/gunicorn-error.log &
+# nohup gunicorn --workers 4 --timeout 120 --keep-alive 5 --bind 0.0.0.0:8080 app:app --access-logfile /root/employee_app/logs/gunicorn-access.log --error-logfile /root/employee_app/logs/gunicorn-error.log &
+
